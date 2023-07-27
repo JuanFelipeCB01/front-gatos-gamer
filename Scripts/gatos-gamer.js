236a7d0f4f1e71996ffc73bbd91de71d401bcaeb
JuanFelipeCB01 <jfelipecbermudez@gmail.com>
2023-07-28 00:26:52 +0200

    bug en appendChild del drawUsers users

--- Scripts/gatos-gamer.js
+++ Scripts/gatos-gamer.js
@@ -1,15 +1,27 @@
-const userList$$ = document.querySelector(".main-users")
+let userList$$ = document.querySelector(".main-users")
+let gamesList$$ = document.querySelector(".main-games")
 const btnUsers$$ = document.querySelector(".users")
 const btnGames$$ = document.querySelector(".games")
 const btnPlatforms$$ = document.querySelector(".platforms")
-
 /* <img src=${user.image} alt="${user.name}" class='card-image'/> */
+
+console.log(userList$$)
 
 const getUsers = async()=>{
     const response = await fetch(`http://localhost:5300/usuarios`);
     const res = await response.json();
-    console.log(res)
+
     return res;
+}
+
+const mapUsers = async(nonMapped)=>{
+    return nonMapped.map((user)=>({
+        name: user.nombre,
+        image: user.foto,
+        email: user.email,
+        favorite: user.favorito,
+        id: user.id
+    }))
 }
 
 const drawUsers = (users)=>{
@@ -33,21 +45,10 @@ const drawUsers = (users)=>{
     }
 }
 
-const mapUsers = async(nonMapped)=>{
-    return nonMapped.map((user)=>({
-        name: user.nombre,
-        image: user.foto,
-        email: user.email,
-        favorite: user.favorito,
-        id: user.id
-    }))
-}
-
 const getGames = async()=>{
     const response = await fetch(`http://localhost:5300/videojuegos`);
     const res = await response.json();
 
-    console.log(res)
     return res;
 }
 
@@ -57,29 +58,47 @@ const mapGames = async(nonMapped)=>{
         image: game.foto,
         releaseDate: game.año,
         developer: game.desarrollador,
-        platforms: game.plataforma,
+        platforms: game.plataformas,
         description: game.descripcion
     }))
 }
 
-const init = async()=>{
-
-    const users = getUsers()
-
-    const mappeUsers = mapUsers(users)
-
-    drawUsers(mappeUsers)
-
-    console.log(mappeUsers)
-
-    const games = getGames()
-
-    console.log(games)
-
-    const mappeGames = mapGames(games)
+const drawGames = (games)=>{
+    for (const game of games){
+        const gameContainer$$ = document.createElement("div")
+        gameContainer$$.setAttribute("class","main-games-item")
+        gameContainer$$.innerHTML =
+        `
+        <h2 class="main-games-item-name">${game.name}</h2>
+        <h3 class="main-games-item-developer">${game.developer}</h3>
+        <p class="main-games-items-description">${game.description}</p>
+        <div class="platforms">
+            <p>${game.platforms[0]}</p>
+            <p>${game.platforms[1]}</p>
+        </div>
+        `
+        gamesList$$.appendChild(gameContainer$$);
+    }
 }
 
-init()
+const showUsers = async()=>{
+    userList$$="flex"
+    gamesList$$.style.display="none"
+    const users = await getUsers()
+    const mappeUsers = await mapUsers(users)
+    drawUsers(mappeUsers)
+}
 
-// btnUsers$$.addEventListener("click", ()=>drawUsers(mappeUsers))
+const showGames = async()=>{
+    userList$$.style.display="none"
+    gamesList$$.style.display="flex"
+    const games = await getGames()
+    const mappeGames = await mapGames(games)
+    console.log(mappeGames)
+    drawGames(mappeGames)
+}
+
+btnUsers$$.addEventListener("click", ()=>showUsers())
+
+btnGames$$.addEventListener("click", ()=>showGames())
 
